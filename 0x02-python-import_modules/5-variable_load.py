fc2f57cd74b6e766c0c5c38ab03f5f03d4621776
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-05 21:20:49 +0000

    Importing a variable from a module

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from variable_load_5 import a
+
+
+def main():
+    """Import and print a variable from a module"""
+    print("{}".format(a))
+
+
+if __name__ == "__main__":
+    main()
