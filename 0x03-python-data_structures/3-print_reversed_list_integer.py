066727a9b06b5dbd687e70de54835e7e4bbbc3ea
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-09 14:04:45 +0000

    Sorting the list first before reversing it and then printing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Prints the integers of a list in reverse order"""
+    sorted(my_list)
     length = len(my_list)
     for i in range(length - 1, -1, -1):
         print("{:d}".format(my_list[i]))
