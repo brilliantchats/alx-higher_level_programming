1a26e4f94906f0e8a920e444786502b839a242ac
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-09 13:43:51 +0000

    print integers in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Print integers in a list"""
+    for intgr in my_list:
+        print("{:d}".format(intgr))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+Python simple data structures - Lists and Tuples
