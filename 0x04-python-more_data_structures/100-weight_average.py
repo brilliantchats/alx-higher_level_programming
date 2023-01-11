a837ce76636a65c48e9f01185ba5550556e8a872
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-11 20:24:17 +0000

    Fix issues with  weighted average script

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,4 +8,6 @@ def weight_average(my_list=[]):
     for tupl in my_list:
         prod_total += (tupl[0] * tupl[1])
         average += tupl[1]
+    if average == 0:
+        return 0
     return prod_total / average
