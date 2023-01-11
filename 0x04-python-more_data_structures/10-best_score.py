0a0c194987f6ac5a1bc75a0829e879784c676892
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-11 16:39:48 +0000

    Fixing a few issues

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@ def best_score(a_dictionary):
     """returns a key with the biggest integer value"""
     if type(a_dictionary) is not dict:
         return None
+    if a_dictionary == {}:
+        return None
     maximum = max(list(a_dictionary.values()))
     for k, v in a_dictionary.items():
         if v == maximum:
