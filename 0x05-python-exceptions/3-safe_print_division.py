daa1dd8ad07b265cb46b307c591aad15dbb59c75
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-23 15:19:30 +0000

    A script that divides 2 integers

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """A function that divides two integers"""
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
