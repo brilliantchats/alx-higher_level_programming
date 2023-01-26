419ece82f00671c4fb35a362a7bc095bdeda7324
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-26 00:47:20 +0000

    A simple python class with a private variable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+'Square' a class that defines a square
+"""
+
+
+class Square:
+    """
+    Defines a square
+    Attributes:
+        size: size of square
+    """
+    def __init__(self, size):
+        """
+        init method for square
+        Args:
+            size: size of the square
+        """
+        self.__size = size
