170645f8f333d39526830b372d3e00f8d725b6a3
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-30 21:05:53 +0000

    static methods in python

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -101,7 +101,7 @@ class Rectangle():
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @staticmethod    
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Compares two rectangle objects based on area"""
         if isinstance(rect_1, Rectangle):
