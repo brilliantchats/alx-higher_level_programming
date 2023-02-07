74978b17b1e08a0f17ecd445b5e06fd3b47ea7c3
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-02-07 22:25:27 +0000

    class Student that defines a student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A class that defines a student
+"""
+
+
+class Student():
+    """
+    Class about Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiates the Student class
+        Attr:
+            first_name: First name of Student
+            last_name: Last name of Student
+            age: Age of Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        if attrs:
+            data = {}
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    data[attr] = self.__dict__[attr]
+            return data
+        else:
+            return self.__dict__
