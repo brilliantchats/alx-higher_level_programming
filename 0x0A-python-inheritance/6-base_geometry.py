c38a2cb28ea4d631a9b89bc5e173052efc363966
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-02-07 01:38:59 +0000

    Adds a public instance method to the BaseGeometry class that simply raises an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry():
+    """
+    Defines a single public instance method
+    """
+    def area(self):
+        """
+        Raises an exception
+        """
+        raise Exception("area() is not implemented")
