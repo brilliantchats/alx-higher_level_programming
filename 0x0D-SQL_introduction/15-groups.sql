8f5d5beb9e6bcef7c0edac2396b49507e0967f23
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-03-16 16:11:04 +0000

    Using GROUP BY and IS NOT in sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- Lists the num of records with the same score in second table
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- Lists all records
-SELECT score, name FROM second_table ORDER BY score DESC
+SELECT score, name
+	FROM second_table
+	WHERE name IS NOT NULL
+	ORDER BY score DESC;
