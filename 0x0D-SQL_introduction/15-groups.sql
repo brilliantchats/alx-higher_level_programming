f1a3b4f7b9b4194fc5f4549d44c7bb4ad3667982
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-03-16 16:43:55 +0000

    order by and group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 -- Lists the num of records with the same score in second table
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score
+SELECT score, COUNT(score) AS number
+	FROM second_table
+       	GROUP BY score
+       	ORDER BY number DESC;
