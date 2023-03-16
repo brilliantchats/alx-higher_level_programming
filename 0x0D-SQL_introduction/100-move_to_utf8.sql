63ced85164cd0be861ee18c93a760cd6cf08b065
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-03-16 18:28:30 +0000

    Change character set and collate for a database, table and a field

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Change charset and collation to utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name
+	VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
