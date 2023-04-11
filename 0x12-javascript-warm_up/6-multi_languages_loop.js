2aabeca5ef3460c8770b6070fd29898983e14281
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-04-11 22:32:17 +0000

    Use of a simple while loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = arr.length;
+let i = 0;
+while (i < len) {
+  console.log(arr[i]);
+  i = i + 1;
+}
