d1e61b225c1b0b12ab9d862ae824f98499e4efda
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-04-19 18:46:26 +0000

    Using the MySQLdb api

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+Connects to a database using the MySQLdb api
+"""
 import sys
 import MySQLdb
 
 
 def main():
+    """Prints the rows from a database"""
     if len(sys.argv) == 4:
         db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3], port=3306)
