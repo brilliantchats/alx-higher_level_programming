c26e024e9debc82db76efeae8e027b7230b5fc34
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-04-19 18:38:45 +0000

    Script that uses the MySQLdb API to connect to a database and execute some basic SQL

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+def main():
+    if len(sys.argv) == 4:
+        db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,8 @@
+0x0F. Python - Object-relational mapping
+
+------------------------------------------------------------------------
+
+This is part of the ALX software engineering curriculum projects.
+It's about Object Relational Mapping.
+It's comprised of the use of Python DB-API in this case, MySQLdb and 
+using the ORM from SQLAlchemy.
