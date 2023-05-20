d13d27d304383e8a2294be58207bf95bbec5ae10
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-05-20 22:53:03 +0000

    Lists states that start with a particular letter using MySQLdb

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ def main():
         db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
         rows = cursor.fetchall()
         for row in rows:
             print(row)
